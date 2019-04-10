9ae6c3d721b5a72f6e5aec3a7526782fa64c104e
omrobbie <putu.roby@yahoo.com>
2019-04-11 05:49:04 +0800

    add closure to bg property

--- GetItDone/AppDelegate.swift
+++ GetItDone/AppDelegate.swift
@@ -16,8 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        print(#function)
-        
         let vc = WelcomeController()
         
         window = UIWindow(frame: UIScreen.main.bounds)
--- GetItDone/WelcomeController.swift
+++ GetItDone/WelcomeController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class WelcomeController: UIViewController {
-
+    
+    let bg:UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        view.layer.cornerRadius = 6
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(#function)
+        view.backgroundColor = .white
         
-        view.backgroundColor = .yellow
+        view.addSubview(bg)
+        bg.topAnchor.constraint(equalTo: view.topAnchor, constant: 20).isActive = true
+        bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20).isActive = true
+        bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20).isActive = true
+        bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
     }
 }
