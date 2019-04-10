33c7fb0a38e7a6ec823eccee0915feead3a70606
omrobbie <putu.roby@yahoo.com>
2019-04-11 06:58:34 +0800

    simple title label with UILabel

--- GetItDone/WelcomeController.swift
+++ GetItDone/WelcomeController.swift
@@ -17,6 +17,8 @@ class WelcomeController: UIViewController {
         return view
     }()
     
+    let titleLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,5 +29,13 @@ class WelcomeController: UIViewController {
         bg.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
         bg.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
         bg.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+        
+        titleLabel.text = "GET IT DONE!"
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleLabel)
+        titleLabel.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: bg.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: bg.topAnchor, constant: 60).isActive = true
     }
 }
