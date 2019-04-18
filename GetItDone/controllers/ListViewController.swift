7ee88bd83897f7ba8d83cf2b6897453945234fbe
omrobbie <putu.roby@yahoo.com>
2019-04-19 01:35:03 +0800

    setup header on list view controller

--- GetItDone/controllers/ListViewController.swift
+++ GetItDone/controllers/ListViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ListViewController: UIViewController {
 
+    let header = GDHeaderView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
+        view.addSubview(header)
+        header.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        header.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        header.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        header.heightAnchor.constraint(equalToConstant: 120).isActive = true
     }
 }
